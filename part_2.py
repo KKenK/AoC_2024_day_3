0e82ff4f518d0fc2f85cc669caf7a54efffcb2a3
Kyle <kylekenrick55@gmail.com>
2025-02-02 18:38:45 +0000

    part_2: working solution committed.

--- part_2.py
+++ part_2.py
@@ -1,13 +1,23 @@
 import input_parser
-import part_1
-
-def append_periods(string, number):
-
-    return string + "." * number
+from part_1 import extract_valid_multiple_pairs
+from part_1 import seven_char_digit_pair_str_checker
+import operator
 
 def get_enabled_code(input):
 
-    input = append_periods(input, 5)
+    input = "do()" + input
+
+    do_indexs = [(x, "do()")for x in find_instruction_indexs(input, "do()")]
+ 
+    dont_indexs = [(x, "don't()")for x in find_instruction_indexs(input, "don't()")]
+
+    sorted_instruction_indexs = sorted(do_indexs + dont_indexs, key= operator.itemgetter(0))
+
+    sorted_instruction_indexs = remove_consecutive_duplicate_commands(sorted_instruction_indexs)
+
+    enabled_input = slice_together_enabled_code_using_instruction_indexs(input, sorted_instruction_indexs)
+
+    return enabled_input
 
 def find_instruction_indexs(input, instuction_string):
 
@@ -21,14 +31,49 @@ def find_instruction_indexs(input, instuction_string):
             continue
 
         instruction_indexs.append(i)
-    
+
     return instruction_indexs
+
+def remove_consecutive_duplicate_commands(sorted_instruction_indexs):
+
+    i = 0
+
+    last_instruction_index = len(sorted_instruction_indexs) - 1
+
+    while i < last_instruction_index:
+
+        if not sorted_instruction_indexs[i][1] == sorted_instruction_indexs[i + 1][1]:
+            i += 1
+            continue
+
+        sorted_instruction_indexs.pop(i + 1)
+
+        last_instruction_index -= 1
+    
+    return sorted_instruction_indexs
+
+def slice_together_enabled_code_using_instruction_indexs(input, sorted_instruction_indexs):
+ 
+    enabled_input = ""
+
+    for i in range(0, len(sorted_instruction_indexs) - 1, 2):
+        
+        enabled_input += input[sorted_instruction_indexs[i][0]: sorted_instruction_indexs[i + 1][0]] 
+
+    if sorted_instruction_indexs[-1][1] == "do()":
+        enabled_input += input[sorted_instruction_indexs[-1][0]: ]
+
+    return enabled_input
 
 if __name__ == "__main__":
 
     input_parser = input_parser.InputParser(r"C:\Users\kylek\Documents\code\Advent_of_code\2024\Day_3\input.txt")
 
+    enabled_input = get_enabled_code(input_parser.parsed_input)
+    
     valid_multiple_pairs = []
+
+    valid_multiple_pairs =extract_valid_multiple_pairs(enabled_input)
 
     total = 0
 
