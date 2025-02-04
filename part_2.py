0ceacea68f207913f5063105190ce31b7a4ac64c
Kyle <kylekenrick55@gmail.com>
2025-02-04 22:22:41 +0000

    part_2: indexs replaced with correct spelling indexes.

--- part_2.py
+++ part_2.py
@@ -7,61 +7,61 @@ def get_enabled_code(input):
 
     input = "do()" + input
 
-    do_indexs = [(x, "do()")for x in find_instruction_indexs(input, "do()")]
+    do_indexes = [(x, "do()")for x in find_instruction_indexes(input, "do()")]
  
-    dont_indexs = [(x, "don't()")for x in find_instruction_indexs(input, "don't()")]
+    dont_indexes = [(x, "don't()")for x in find_instruction_indexes(input, "don't()")]
 
-    sorted_instruction_indexs = sorted(do_indexs + dont_indexs, key= operator.itemgetter(0))
+    sorted_instruction_indexes = sorted(do_indexes + dont_indexes, key= operator.itemgetter(0))
 
-    sorted_instruction_indexs = remove_consecutive_duplicate_commands(sorted_instruction_indexs)
+    sorted_instruction_indexes = remove_consecutive_duplicate_commands(sorted_instruction_indexes)
 
-    enabled_input = slice_together_enabled_code_using_instruction_indexs(input, sorted_instruction_indexs)
+    enabled_input = slice_together_enabled_code_using_instruction_indexes(input, sorted_instruction_indexes)
 
     return enabled_input
 
-def find_instruction_indexs(input, instuction_string):
+def find_instruction_indexes(input, instuction_string):
 
     instuction_string_length = len(instuction_string)
 
-    instruction_indexs = []
+    instruction_indexes = []
 
     for i in range(0, len(input) - instuction_string_length):
 
         if not input[i : i + instuction_string_length] == instuction_string:
             continue
 
-        instruction_indexs.append(i)
+        instruction_indexes.append(i)
 
-    return instruction_indexs
+    return instruction_indexes
 
-def remove_consecutive_duplicate_commands(sorted_instruction_indexs):
+def remove_consecutive_duplicate_commands(sorted_instruction_indexes):
 
     i = 0
 
-    last_instruction_index = len(sorted_instruction_indexs) - 1
+    last_instruction_index = len(sorted_instruction_indexes) - 1
 
     while i < last_instruction_index:
 
-        if not sorted_instruction_indexs[i][1] == sorted_instruction_indexs[i + 1][1]:
+        if not sorted_instruction_indexes[i][1] == sorted_instruction_indexes[i + 1][1]:
             i += 1
             continue
 
-        sorted_instruction_indexs.pop(i + 1)
+        sorted_instruction_indexes.pop(i + 1)
 
         last_instruction_index -= 1
     
-    return sorted_instruction_indexs
+    return sorted_instruction_indexes
 
-def slice_together_enabled_code_using_instruction_indexs(input, sorted_instruction_indexs):
+def slice_together_enabled_code_using_instruction_indexes(input, sorted_instruction_indexes):
  
     enabled_input = ""
 
-    for i in range(0, len(sorted_instruction_indexs) - 1, 2):
+    for i in range(0, len(sorted_instruction_indexes) - 1, 2):
         
-        enabled_input += input[sorted_instruction_indexs[i][0]: sorted_instruction_indexs[i + 1][0]] 
+        enabled_input += input[sorted_instruction_indexes[i][0]: sorted_instruction_indexes[i + 1][0]] 
 
-    if sorted_instruction_indexs[-1][1] == "do()":
-        enabled_input += input[sorted_instruction_indexs[-1][0]: ]
+    if sorted_instruction_indexes[-1][1] == "do()":
+        enabled_input += input[sorted_instruction_indexes[-1][0]: ]
 
     return enabled_input
 
